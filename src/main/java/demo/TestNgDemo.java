5339c158da67fa123412100b13e5eaadf11ca35b
mithun-reddy <mithun.r@tekarch.com>
2023-03-29 11:22:01 -0700

    Modified TestNG file

--- src/main/java/demo/TestNgDemo.java
+++ src/main/java/demo/TestNgDemo.java
@@ -6,9 +6,6 @@ import org.testng.annotations.Test;
 
 public class TestNgDemo {
 	
-//	Default priority as 0
-//	A-Z, a-z, 0-9
-//	Priorities can be from -ve to +ve
 	@Test(priority = 1)
 	public void login1() {
 		System.out.println("home ... ");
@@ -17,7 +14,6 @@ public class TestNgDemo {
 	@Test(priority = 1)
 	public void login2() {
 		System.out.println("login .... ");
-//		throw new NoSuchElementException();
 	}
 	
 	@Test (priority = 1)
