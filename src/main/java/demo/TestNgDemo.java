d3cb346a33630a7484a8281eba79023c9f3f853a
mithun-reddy <mithun.r@tekarch.com>
2023-03-30 10:59:59 -0700

    Improved readbility

--- src/main/java/demo/TestNgDemo.java
+++ src/main/java/demo/TestNgDemo.java
@@ -6,24 +6,24 @@ import org.testng.annotations.Test;
 
 public class TestNgDemo {
 	
-	@Test()
+	@Test(priority = 1)
 	public void login1() {
-		System.out.println("home ... ");
+		System.out.println("home page ... ");
 	}
 	
-	@Test()
+	@Test(priority = 2)
 	public void login2() {
-		System.out.println("login .... ");
+		System.out.println("login page .... ");
 	}
 	
-	@Test (priority = 3)
+	@Test (priority = 4)
 	public void accounts() {
-		System.out.println("accounts.... ");
+		System.out.println("accounts page.... ");
 	}
 	
-	@Test (priority = -1)
+	@Test (priority = 0)
 	public void opportunities() {
-		System.out.println("opportunities... ");
+		System.out.println("opportunities page... ");
 	}
 	
 }
