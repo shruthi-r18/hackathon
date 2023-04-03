69f6e2aaca22c817ca6489e9c16970e4d35b6819
mithun-reddy <mithun.r@tekarch.com>
2023-04-03 09:20:08 -0700

    Added first data driven commit

--- src/main/java/demo/TestNgDemo.java
+++ src/main/java/demo/TestNgDemo.java
@@ -1,14 +1,20 @@
 package demo;
 
+import java.io.IOException;
 import java.util.NoSuchElementException;
 
 import org.testng.annotations.Test;
 
+import constants.FileConstants;
+import file.utils.PropertiesFileutils;
+
 public class TestNgDemo {
 	
 	@Test(priority = 3)
-	public void login1() {
+	public void login1() throws IOException {
 		System.out.println("home page ... ");
+		
+		String sUsername = PropertiesFileutils.readPropertiesFile(FileConstants.CREDENTIALS_FILE_PATH, "dev.username");
 	}
 	
 	@Test(priority = 2)
---
+++ src/main/java/file/utils/ExcelUtils.java
@@ -0,0 +1,24 @@
+package file.utils;
+
+
+/**
+ * @author Mithun reddy 
+ *	This class will have all the reusable utils for an excel file system
+ */
+public class ExcelUtils {
+	
+	
+	/**
+	 * @param filePath - Provide filepath of the excel to be read 
+	 * @param row - provide the row number to read should be a integer
+	 * @param cell - provide the cell number to be read 
+	 * @return String value of the cell
+	 */
+	public static String readExcel(String filePath, int row, int cell) {
+	
+		
+		
+		return "Value";
+	}
+
+}
